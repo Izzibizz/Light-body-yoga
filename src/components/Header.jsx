e28059af6c67db2e0b83676627cc9889c618597c
izzibizz <contact@izabellind.com>
2025-02-09 13:12:49 +0100

    header responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,136 @@
-
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 
 export const Header = () => {
+
+  const dropdownRef = useRef();
+  const buttonRef = useRef();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target)
+      ) {
+        closeMenu();
+      }
+    };
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Clean up the event listener
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div>
-      
-    </div>
+    <header className="fixed top-0 z-20 w-full bg-light/10 py-4 flex px-4 laptop:px-16 ">
+      <div className="flex w-full justify-between laptop:hidden">
+      <img src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1736872757/lby-logga-text_vgk1uh.svg"  alt="Therese Lind Bjellder Light Body Yoga" className="w-[100px] justify-self-center cursor-pointer"/>
+       <button
+        ref={buttonRef}
+        onClick={toggleMenu}
+        aria-label="Toggle Menu"
+        className="flex flex-col justify-center items-center z-50 cursor-pointer justify-self-end"
+      >
+        <span
+          className={`bg-stone-900 block transition-all duration-300 ease-out 
+                      h-0.5 w-6 laptop:h-0.7 laptop:w-6 rounded-sm ${
+                        isOpen
+                          ? "rotate-45 translate-y-1 laptop:translate-y-1.2 bg-warm-white"
+                          : "-translate-y-0.5 laptop:-translate-y-0.7 bg-stone-900"
+                      }`}
+        >
+          {" "}
+        </span>
+        <span
+          className={` bg-stone-900 block transition-all duration-300 ease-out 
+                      h-0.5 w-6 laptop:h-0.7 laptop:w-6 rounded-sm my-0.5 laptop:my-0.7 ${
+                        isOpen ? "opacity-0" : "opacity-100"
+                      }`}
+        ></span>
+        <span
+          className={`bg-stone-900  block transition-all duration-300 ease-out 
+                      h-0.5 w-6 laptop:h-0.7 laptop:w-6 rounded-xl ${
+                        isOpen
+                          ? "-rotate-45 -translate-y-1 laptop:-translate-y-1.2 bg-warm-white"
+                          : "translate-y-0.5 laptop:translate-y-0.7 bg-stone-900"
+                      }`}
+        ></span>
+      </button>
+      </div>
+      {isOpen && (
+        <div
+          ref={dropdownRef}
+          className={`absolute laptop:top-4 laptop:right-12 top-0 z-40 right-0 w-full h-screen laptop:w-fit laptop:h-fit text-xl text-warm-white bg-dark-blue/90 laptop:bg-warm-white/0 flex justify-center `}
+        >
+          <ul className="flex flex-col laptop:flex-row items-center align-middle laptop:items-end gap-6 laptop:gap-10 laptop:pb-4 laptop:px-6 font-light mt-48 laptop:mt-0">
+            <li
+            /*   onClick={() => handleNavClick("galleri")} */
+              className={`hover:scale-110 hover:text-dark-brown transform transition-transform duration-300 origin-center cursor-pointer`}
+            >
+              Schedule
+            </li>
+            <li
+             /*  onClick={() => handleNavClick("omEllen")} */
+              className={`hover:scale-110 hover:text-dark-brown transform transition-transform duration-300 origin-center cursor-pointer`}
+            >
+              Workshops
+            </li>
+            <li
+             /*  onClick={() => handleNavClick("cv")} */
+              className={`hover:scale-110 hover:text-dark-brown transform transition-transform duration-300 origin-center cursor-pointer`}
+            >
+              Office yoga
+            </li>
+            <li
+             /*  onClick={() => handleNavClick("cv")} */
+              className={`hover:scale-110 hover:text-dark-brown transform transition-transform duration-300 origin-center cursor-pointer`}
+            >
+              Treatments
+            </li>
+            <li
+             /*  onClick={() => handleNavClick("cv")} */
+              className={`hover:scale-110 hover:text-dark-brown transform transition-transform duration-300 origin-center cursor-pointer`}
+            >
+              About
+            </li>
+            <li
+             /*  onClick={() => handleNavClick("contact")} */
+              className={`hover:scale-110 hover:text-dark-brown transform transition-transform duration-300 origin-center cursor-pointer`}
+            >
+              Contakt
+            </li>
+          </ul>
+        </div>
+      )}
+      <div className="hidden laptop:flex justify-between w-full items-center">
+      <div className="flex justify-between gap-32 text-sm">
+      <span className="ml-4">Schedule</span>
+      <span className="ml-4">Workshops</span>
+      <span className="ml-4">Office Yoga</span>
+      </div>
+      <img src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1736872757/lby-logga-text_vgk1uh.svg"  alt="Therese Lind Bjellder Light Body Yoga" className="w-[100px] cursor-pointer"/>
+      <div className="flex justify-between gap-32 text-sm">
+      <span className="mr-4">Treatments</span>
+      <span className="mr-4">About</span>
+      <span className="mr-4">Contact</span>
+      </div>
+      </div>
+    </header>
   )
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -17,25 +17,12 @@ export const LandingPage = () => {
           type="video/mp4"
         />
       </video>
-      <div className="absolute top-0 z-20 w-full bg-light bg-opacity-10 py-4 flex justify-between items-center px-16">
-      <div className="flex justify-between gap-32 font-body text-sm">
-      <span className="ml-4">Schedule</span>
-      <span className="ml-4">Workshops</span>
-      <span className="ml-4">Office Yoga</span>
-      </div>
-      <img src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1736872757/lby-logga-text_vgk1uh.svg"  alt="Therese Lind Bjellder Light Body Yoga" className="w-[100px]"/>
-      <div className="flex justify-between gap-32 font-body">
-      <span className="mr-4">Treatments</span>
-      <span className="mr-4">About</span>
-      <span className="mr-4">Contact</span>
-      </div>
-      </div>
       <div className="relative top-5 w-full flex z-40 flex flex-col">
         
-      <div className="flex w-10/12 mx-auto mt-32 justify-end">
-        <img src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1736928855/530be1ab3b6184dd15e0e5a0b6012ac8-22_gebuz4.jpg" className=" w-[700px] rounded-xl"></img>
-        <img src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1736868251/svg_test_text_uohmza.svg" className="absolute w-[750px] left-1/4 top-1/3"/>
-        <div className="flex font-body items-center gap-2 border border-black rounded-2xl py-1 px-3 w-fit ml-40 absolute left-[380px] top-[80%] ">Read more <MdArrowDropDownCircle /></div>
+      <div className="flex flex-col laptop:flex-row w-10/12 mx-auto mt-32 laptop:justify-end items-center gap-4">
+        <img src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1736928855/530be1ab3b6184dd15e0e5a0b6012ac8-22_gebuz4.jpg" alt="Therese Lind Bjellder" className="w-full tablet:w-2/3 tablet:self-end laptop:w-1/2 rounded-xl"></img>
+        <img src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1739100255/svg_test-text_nmxvhb.svg" alt="text" className="tablet:absolute w-full tablet:w-1/2 laptop:w-[40%] [750px] tablet:left-10 tablet:top-2/3 laptop:left-[20%] laptop:top-1/3"/>
+        <div className="flex font-body items-center gap-2 border border-black rounded-2xl py-1 px-3 w-fit tablet:self-end tablet:mr-20 laptop:mr-0 laptop:ml-40 laptop:absolute laptop:left-[20%] laptop:bottom-0 ">Read more <MdArrowDropDownCircle /></div>
       </div>
       
       </div>
