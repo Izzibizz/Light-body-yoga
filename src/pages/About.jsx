966413060c4abd10fc82eb7ee6042e4483027845
izzibizz <contact@izabellind.com>
2025-02-13 15:02:08 +0100

    fixed animation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 
 export const About = () => {
     return (
-      <section >
+      <section className="animate-mediumFadeIn ">
       <div className="relative relative w-9/12 tablet:w-10/12 laptop:w-9/12 mx-auto mt-60 flex flex-col text-stone-900">
        <h3 className="font-dream text-3xl">About me</h3> </div>
       </section>
--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -5,7 +5,7 @@ export const Schedule = () => {
   
 
     return (
-      <section >
+      <section className="animate-mediumFadeIn ">
       <div className="relative relative w-9/12 tablet:w-10/12 laptop:w-9/12 mx-auto mt-60 flex flex-col text-stone-900">
        <h3 className="font-dream text-3xl">Schedule</h3> </div>
       </section>
