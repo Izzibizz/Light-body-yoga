d4b0d36a749d62d5056948089fd16f21ee01ef2d
izzibizz <contact@izabellind.com>
2025-03-27 17:06:56 +0100

    about layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,69 +1,101 @@
 export const About = () => {
   return (
     <section className="animate-mediumFadeIn ">
-      <div className="relative w-9/12 tablet:w-10/12 laptop:w-9/12 mx-auto mt-40 laptop:mt-48 flex flex-col text-stone-900 gap-10">
+      <div className="relative w-9/12 tablet:w-10/12 laptop:w-9/12 mx-auto mt-40 laptop:mt-42 flex flex-col text-stone-900 gap-10">
         <div className="flex flex-col laptop:flex-row gap-16">
           <div className="flex flex-col gap-10">
-          <h3 className="font-dream text-3xl">About me</h3>
-          <img
-            src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1742493989/skissbild_Therese_Y8A8247-1-srgb_cjpu0u.jpg"
-            alt="therese lind bjellder Light body yoga"
-            className="laptop:max-w-[500px] aspect-[3/4] object-cover rounded-2xl tablet:hidden"
-          />
-          <div className="flex flex-col gap-4 text-justify">
-            <p>
-              Hi I’m Therese! My journey to yoga, and back to embodying my
-              healer’s soul in this lifetime, has been a winding, rocky and
-              beautiful road that’s carried me across the world—a path of
-              movement, creativity, and deep healing, paved with self-discovery
-              and growth.
-            </p>
-            <p>
-              I began my path in 2002 at the Ballet Academy in Stockholm, where
-              I trained to become a dancer. For years, I worked as a dancer and
-              musical theatre artist, expressing my soul and inner core through
-              the language of my body—a passion that still defines me. My
-              favorite quote is by Isadora Duncan, “You were once wild here.
-              Don’t let them tame you,” and I carry that untamed spirit into
-              everything I do.
-            </p>
-            <p>
-              Back in 2013 my life took me to New York, where I lived for five
-              years, studying acting and discovering yoga—a turning point that
-              reshaped my world. I pursued 300 hours of yoga teacher training in
-              Costa Rica and 200 more in Bali, immersing myself in the practice,
-              philosophy, and tools of yoga. Later I deepened my teacher
-              training in Yin yoga here in Stockholm, my base since 5 years
-              back. Through meditation and these teachings, I found profound
-              healing, and they now form the foundation of my life.
-            </p>
-            <p>
-              Today, I’m dedicated to helping others tap into their inner
-              strength through yoga. With a heart full of light and love, I
-              share dynamic, playful classes infused with themes and intentions
-              to cultivate energy and mindfulness. My creativity flows into
-              every session, reflecting my dancer’s soul. Beyond teaching yoga,
-              I offer healing through reiki and massage, guiding others toward
-              authenticity and compassion—for themselves, towards others, and
-              how we show up for Mother Earth.
-            </p>
-            <p>
-              Yoga, to me, is more than a practice; it’s a way to unlock
-              insights, heal, and become more observant and empathic. My mission
-              is to inspire that journey in others, drawing from my lifelong
-              love of movement and the wisdom I’ve gained along the way. Whether
-              in the studio or beyond, I’m here to share the transformative
-              power of yoga with you.
-            </p>
+            <div className="flex gap-4 items-center">
+              <h3 className="font-dream text-3xl">About me</h3>
+              <img
+                src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1743091266/blomma_ad2dpv.svg"
+                alt="balance symbol"
+                className="w-[40px] h-[40px]"
+              />
+            </div>
+            <img
+              src="https://res.cloudinary.com/dbf8xygxz/image/upload/t_w-500/v1742493989/skissbild_Therese_Y8A8247-1-srgb_cjpu0u.jpg"
+              alt="therese lind bjellder Light body yoga"
+              className="aspect-[3/4] object-cover rounded-2xl tablet:hidden"
+            />
+            <div className="flex flex-col gap-4 text-justify laptop:grid laptop:grid-cols-3 laptop:gap-x-20">
+              <div className="flex flex-col gap-4">
+                <h4 className="font-dream text-2xl">
+                  <span className="font-medium">Hi,</span> I’m Therese!
+                </h4>
+                <p>
+                  My journey to yoga, and back to embodying my healer’s soul in
+                  this lifetime, has been a winding, rocky and beautiful road
+                  that’s carried me across the world—a path of movement,
+                  creativity, and deep healing, paved with self-discovery and
+                  growth.
+                </p>
+                <div className="flex gap-10">
+                  <img
+                    src="https://res.cloudinary.com/dbf8xygxz/image/upload/t_w-500/v1742493989/skissbild_Therese_Y8A8247-1-srgb_cjpu0u.jpg"
+                    alt="therese lind bjellder Light body yoga"
+                    className="max-w-[300px] h-[400px] rounded-2xl hidden tablet:block laptop:hidden"
+                  />
+                  <div>
+                    <h4 className="font-dream text-2xl self-end laptop:self-start">
+                      How it began
+                    </h4>
+                    <p>
+                      I began my path in 2002 at the Ballet Academy in
+                      Stockholm, where I trained to become a dancer. For years,
+                      I worked as a dancer and musical theatre artist,
+                      expressing my soul and inner core through the language of
+                      my body—a passion that still defines me. My favorite quote
+                      is by Isadora Duncan, “You were once wild here. Don’t let
+                      them tame you,” and I carry that untamed spirit into
+                      everything I do.
+                    </p>
+                    <p>
+                      Back in 2013 my life took me to New York, where I lived
+                      for five years, studying acting and discovering yoga—a
+                      turning point that reshaped my world. I pursued 300 hours
+                      of yoga teacher training in Costa Rica and 200 more in
+                      Bali, immersing myself in the practice, philosophy, and
+                      tools of yoga. Later I deepened my teacher training in Yin
+                      yoga here in Stockholm, my base since 5 years back.
+                      Through meditation and these teachings, I found profound
+                      healing, and they now form the foundation of my life.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <img
+                src="https://res.cloudinary.com/dbf8xygxz/image/upload/t_w-500/v1742493989/skissbild_Therese_Y8A8247-1-srgb_cjpu0u.jpg"
+                alt="therese lind bjellder Light body yoga"
+                className="laptop:max-w-[300px] laptop:max-h-[400px] rounded-2xl hidden laptop:block justify-self-center"
+              />
+              <div className="flex flex-col gap-4">
+                <h4 className="font-dream text-2xl self-end laptop:self-start mt-4 laptop:mt-0">
+                  How my practice looks today
+                </h4>
+                <p>
+                  Today, I’m dedicated to helping others tap into their inner
+                  strength through yoga. With a heart full of light and love, I
+                  share dynamic, playful classes infused with themes and
+                  intentions to cultivate energy and mindfulness. My creativity
+                  flows into every session, reflecting my dancer’s soul. Beyond
+                  teaching yoga, I offer healing through reiki and massage,
+                  guiding others toward authenticity and compassion—for
+                  themselves, towards others, and how we show up for Mother
+                  Earth.
+                </p>
+                <p className="italic">
+                  Yoga, to me, is more than a practice; it’s a way to unlock
+                  insights, heal, and become more observant and empathic. My
+                  mission is to inspire that journey in others, drawing from my
+                  lifelong love of movement and the wisdom I’ve gained along the
+                  way. Whether in the studio or beyond, I’m here to share the
+                  transformative power of yoga with you.
+                </p>
+              </div>
             </div>
           </div>
-          <img
-            src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1742493989/skissbild_Therese_Y8A8247-1-srgb_cjpu0u.jpg"
-            alt="therese lind bjellder Light body yoga"
-            className="laptop:max-w-[500px] aspect-[3/4] object-cover rounded-2xl hidden tablet:block"
-          /></div>
-          
         </div>
+      </div>
     </section>
   );
 };
