c363150cbc9e93d74bb5a393cb7a543aa766c7df
izzibizz <contact@izabellind.com>
2025-03-28 16:13:32 +0100

    fixed width

--- src/pages/Retreats.jsx
+++ src/pages/Retreats.jsx
@@ -3,7 +3,7 @@
 export const Retreats = () => {
   return (
     <section className="animate-mediumFadeIn ">
-    <div className="relative w-9/12 tablet:w-10/12 laptop:w-9/12 mx-auto mt-40 laptop:mt-48 flex flex-col text-stone-900">
+    <div className="relative w-10/12 laptop:w-9/12 mx-auto mt-40 laptop:mt-48 flex flex-col text-stone-900">
      <h3 className="font-dream text-3xl">Yoga Retreats</h3> </div>
     </section>
   )
