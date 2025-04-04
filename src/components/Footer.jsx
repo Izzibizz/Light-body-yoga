854daed39d23f4df911cd457b009aa137e7ce30c
izzibizz <contact@izabellind.com>
2025-04-04 11:07:42 +0200

    target

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,16 @@ export const Footer = () => {
       <div className="flex flex-col-reverse tablet:flex-row gap-2 tablet:gap-6">
      <a
               href="http://izabellind.com"
+               target="_blank"
+               rel="noopener noreferrer"
               className="relative flex gap-2 items-center text-[8px] after:content-[''] after:block after:w-0 after:h-[1px] after:bg-warm-white after:absolute after:left-0 after:bottom-0 after:transition-all after:duration-300 hover:after:w-full"
             > <MdOutlineArrowOutward />
               Design & Frontend development 2025 | Izabel Lind
             </a>
             <a
               href="https://www.studiomamama.se/"
+              target="_blank"
+               rel="noopener noreferrer"
               className="relative flex gap-2 items-center text-[8px] after:content-[''] after:block after:w-0 after:h-[1px] after:bg-warm-white after:absolute after:left-0 after:bottom-0 after:transition-all after:duration-300 hover:after:w-full"
             > <MdOutlineArrowOutward />
               Fotograf | Studio Mamama
