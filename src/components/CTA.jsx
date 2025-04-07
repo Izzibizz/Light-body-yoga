2b24401745e00c0b2836f99fabf8027237e9f8cb
izzibizz <contact@izabellind.com>
2025-04-07 18:21:23 +0200

    show on scroll down CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react"
 
 export const CTA = () => {
+
+const [ showCTA, setShowCTA ] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+          if (window.scrollY > 0) {
+            setShowCTA(true);
+          } else {
+            setShowCTA(false);
+          }
+        };
+    
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, []);
+
   return (
-    <a href="mailto:therese@lightbodyyoga.se"  className="fixed bottom-6 right-6 tablet:bottom-10 tablet:right-10 z-40 w-fit h-fit bg-orange-500/80 rounded-xl p-2 px-4 tablet:text-lg text-center laptop:hover:scale-105">
+    <>
+    {showCTA && (
+    <a href="mailto:therese@lightbodyyoga.se"  className="animate-shortFadeIn fixed bottom-6 right-6 tablet:bottom-10 tablet:right-10 z-40 w-fit h-fit bg-orange-500/80 rounded-xl p-2 px-4 tablet:text-lg text-center laptop:hover:scale-105">
         Connect with me
     </a>
+  )}
+  </>
   )
 }
 
